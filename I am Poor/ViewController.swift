4465776a327c94e3f10f8b406c7d7b56fffb3b27
Astha Mishra <52097499+ASTHAMISHRA4@users.noreply.github.com>
2022-01-05 01:13:57 +0530

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Astha Mishra on 1/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
